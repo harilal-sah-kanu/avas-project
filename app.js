62f05def9a5686a91f144b52dc2c25b1938c9be9
Harilal Sah Kanu <harilalsahk@gmail.com>
2025-11-14 23:04:12 +0530

    Homepage added

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.redirect("/listings");
+  res.render("home.ejs");
 });
 
 // ------------------ API Connections-----------------------
